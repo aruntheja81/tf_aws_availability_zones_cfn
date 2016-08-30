1c3c3ec94e225621aaadcd869161735ee731570f
Ivan Casco <ivan@ivancasco.com>
2016-08-30 22:16:48 +0200

    Use proper terraform interpolation syntax

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ resource "aws_cloudformation_stack" "availability_zones" {
 }
 
 output "primary" {
-    value = "${ element(split(",", aws_cloudformation_stack.outputs.AvailabilityZones), 0) }"
+    value = "${ element(split(",", aws_cloudformation_stack.availability_zones.outputs.AvailabilityZones), 0) }"
 }
 output "secondary" {
-    value = "${ element(split(",", aws_cloudformation_stack.outputs.AvailabilityZones), 1) }"
+    value = "${ element(split(",", aws_cloudformation_stack.availability_zones.outputs.AvailabilityZones), 1) }"
 }
 output "tertiary" {
-    value = "${ element(split(",", aws_cloudformation_stack.outputs.AvailabilityZones), 2) }"
+    value = "${ element(split(",", aws_cloudformation_stack.availability_zones.outputs.AvailabilityZones), 2) }"
 }
 output "list_all" {
-    value = "${aws_cloudformation_stack.outputs.AvailabilityZones}"
+    value = "${aws_cloudformation_stack.availability_zones.outputs.AvailabilityZones}"
 }
